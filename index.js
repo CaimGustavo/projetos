33a285ce8827c69af3371a4f83b2809ad6098607
CaimGustavo <148461972+CaimGustavo@users.noreply.github.com>
2024-05-30 13:29:27 -0300

    update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ class MobileNavbar{
   this.navList=document.querySelector(navList)
   this.navlinks=document.querySelectorAll(navlinks)
   this.activeClass="active"
+  this.Click=this.Click.bind()
   }
 Click(){
 this.navList.classList.toggle(this.activeClass)  
