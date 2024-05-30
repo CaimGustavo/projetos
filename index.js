2982beef91fec8dc766fe692bd5bff83b9461e35
CaimGustavo <148461972+CaimGustavo@users.noreply.github.com>
2024-05-30 14:02:35 -0300

    update index.js

--- index.js
+++ index.js
@@ -42,9 +42,7 @@ class MobileNavbar{
 animateLinks(){
   console.log(window)
 this.navlinks.forEach((link)=>{
-  link.style.animation
-    ? (link.style.animation = "")
-    :(link.style.animation =`navLinkFade 0.5s ease forwards 0.3s;`);
+  link.style.animation?(link.style.animation = ""):(link.style.animation =`navLinkFade 0.5s ease forwards 0.3s;`)
 
 })  
 }
