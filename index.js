a096529993205389fe450274c2231b7d3fe35f56
CaimGustavo <148461972+CaimGustavo@users.noreply.github.com>
2024-06-06 17:05:27 -0300

    update index.js

--- index.js
+++ index.js
@@ -87,3 +87,19 @@ const mobileNavbar= new MobileNavbar(
 ".nav-list li",
   )
 mobileNavbar.init()
+
+const ctx = document.getElementById('myChart')
+new Chart(ctx, { 
+  type: 'bar', 
+  data: { 
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [{ 
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1 
+  }] 
+    
+  },options: { 
+    scales: {
+    y: { 
+    beginAtZero: true } } } });
