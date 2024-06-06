828cd4085af71625237cc96ea64ac4b229257105
CaimGustavo <148461972+CaimGustavo@users.noreply.github.com>
2024-06-06 17:19:11 -0300

    update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const ctx = document.getElementById('myChart')
 new Chart(ctx, { 
   type: 'bar', 
   data: { 
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Soja', 'feijão', 'milho', 'arroz', 'uva', 'batata'],
   datasets: [{ 
     label: '# of Votes',
     data: [12, 19, 3, 5, 2, 3],
